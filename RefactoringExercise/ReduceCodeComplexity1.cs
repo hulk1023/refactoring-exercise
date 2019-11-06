1125f760b9677d667fa7a9f03a9ef24029a3daca
mengkeong <mengkeong@TI01BY36>
2019-11-06 13:31:51 +0800

    added a case

---
+++ RefactoringExercise/ReduceCodeComplexity1.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace RefactoringExercise
+{
+    [TestFixture()]
+    public class ReduceCodeComplexity1
+    {
+
+        static int CalculateControlDigit(long number)
+        {
+
+            int sum = 0;
+            int pos = 1;
+
+            do
+            {
+
+                int digit = (int)(number % 10);
+
+                if (pos % 2 == 0)
+                {
+                    sum += 3 * digit;
+                }
+                else
+                {
+                    sum += digit;
+                }
+
+                pos++;
+
+                number /= 10;
+            }
+            while (number > 0);
+
+            int result = sum % 11;
+            if (result == 10)
+                result = 1;
+
+            return result;
+
+        }
+
+
+        [Test]
+        public void Test()
+        {
+            var result = CalculateControlDigit(82712476);
+            Assert.AreEqual(8, result);
+        }
+
+
+    }
+
+}
