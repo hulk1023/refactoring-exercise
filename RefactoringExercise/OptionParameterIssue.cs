430f3ecc06802d65384f3219247a0f07c17c39a2
mengkeong <mengkeong@TI01BY36>
2019-11-07 09:56:39 +0800

    added new case

---
+++ RefactoringExercise/OptionParameterIssue.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace RefactoringExercise
+{
+    [TestFixture]
+    class OptionParameterIssue
+    {
+        static decimal GetItemPrice(string itemName, decimal relativeDiscount = 0)
+        {
+
+            if (relativeDiscount < 0 || relativeDiscount >= 1)
+            {
+                throw new ArgumentException("Incorrect discount.", "relativeDiscount");
+            }
+
+            if (relativeDiscount > 0)
+            {
+                Console.WriteLine("LOG Discount {0}% applied.", 100 * relativeDiscount);
+            }
+
+            return 100.0M * itemName.Length * (1.0M - relativeDiscount);
+
+        }
+
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine(GetItemPrice("laptop", .1M));
+            Console.WriteLine(GetItemPrice("pen"));
+
+        }
+    }
+}
