be24a79d3a1e9240c985655b6c406556dd8823ff
Hulk Wang <hulk.wang@titansoft.com.sg>
2019-11-22 13:21:07 +0800

    change method signature of LabelFormatOfAddress 1) return value type as AddressFormat 2) remove "static" decorator

--- RefactoringExercise/Exercise3/ShippingLabelPrinter.cs
+++ RefactoringExercise/Exercise3/ShippingLabelPrinter.cs
@@ -25,7 +25,7 @@ namespace RefactoringExercise.Exercise3
             }
         }
 
-        private static List<string> LabelFormatOfAddress(Address address)
+        private AddressFormat LabelFormatOfAddress(Address address)
         {
             var addressFormat = new AddressFormat();
             addressFormat.Font = "Times New Roman";
@@ -66,7 +66,7 @@ namespace RefactoringExercise.Exercise3
                 addressFormat.Lines.Add(address.District.ToUpper());
             addressFormat.Lines.Add(address.PostalCode);
             addressFormat.Lines.Add(address.Country.ToUpper());
-            return lines;
+            return addressFormat;
         }
     }
 
